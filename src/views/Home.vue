32e7bd421d27e84fc1e30bafef35795dea622d80
Aqshola <aqsholafid2@gmail.com>
2021-05-19 09:57:44 +0700

    fix layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-gradient-main">
+  <div class="bg-gradient-main" style="min-height:90vh">
     <b-container fluid>
-      <b-row>
-        <b-col sm class="p-0 d-block d-md-none ">
+      <b-row class="d-flex flex-lg-row flex-column">
+        <b-col sm class="p-0 d-block d-lg-none ">
           <figure class="figure">
             <img
               src="../assets/BannerComplete.png"
@@ -12,18 +12,18 @@
           </figure>
         </b-col>
         <b-col sm class="col-sm p-5 d-flex flex-column justify-content-center">
-          <h1 class="text-left text-dark text-center text-md-left title-text">
+          <h1 class="text-left text-dark text-center text-lg-left title-text">
             <b>Find</b> all your favorite <b>Pokemon</b>
           </h1>
           <p
-            class="w-md-75 text-left mt-3 text-dark lead text-center text-md-left title-desc"
+            class="w-lg-75 text-left mt-3 text-dark lead text-center text-lg-left title-desc"
           >
             You can know the type of Pokemon, its strengths, disadvantages and
             abilities
           </p>
-          <button class="btn-main mx-auto mx-md-0 ">See pokemons</button>
+          <button class="btn-main mx-auto mx-lg-0 ">See pokemons</button>
         </b-col>
-        <b-col sm class="p-0 d-none d-md-block p-2 p-md-0">
+        <b-col sm class="p-0 d-none d-lg-block p-2 p-md-0">
           <figure class="figure">
             <img
               src="../assets/banner.png"
